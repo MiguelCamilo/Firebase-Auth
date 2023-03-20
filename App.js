dddff0d5b4f5286053135fbd222d09ee5dc7e6e1
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-20 12:42:49 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./src/screens/HomeScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// nav imports
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+
+				<Stack.Screen
+          // removes header
+					options={{ headerShown: false }}
+					name="Login"
+					component={LoginScreen}
+				/>
+				<Stack.Screen name="Home" component={HomeScreen} />
+
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+  return (
+    <KeyboardAvoidingView 
+        style={styles.container}
+        behavior="padding"
+    >
+      
+    </KeyboardAvoidingView>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
