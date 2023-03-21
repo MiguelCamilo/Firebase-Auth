2a585a9a09702a18ddea03d31bf1063ea3ac317c
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-21 00:13:54 -0400

    Completed the register and login button and navigation to home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,12 +1,48 @@
-import { Text, View, KeyboardAvoidingView } from "react-native";
+import { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Text, View, KeyboardAvoidingView, TouchableOpacity } from "react-native";
 import React from "react";
 
+// google authentication
+import { auth } from '../../firebase'
+import { signOut } from "firebase/auth";
+
+
 const HomeScreen = () => {
+
+    const navigation = useNavigation()
+
+    // useEffect(() => {
+    //     const unsubscribe = auth.onAuthStateChanged(user => {
+    //         if (user) {
+    //             navigation.navigate("Login")
+    //         }
+    //     })
+    //     // returning unsubscribe here stops the listener from being called
+    //     return unsubscribe
+    // },[]) 
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                navigation.replace("Login")
+                alert("You have successfully signed out")
+            })
+            .catch(err => console.error(err))
+    }
+
 	return (
 		<KeyboardAvoidingView className="flex-1 justify-center items-center" behavior="padding">
 			<View>
-				<Text>Home Screen</Text>
+                {/* reason for question mark is because email can be undefined which can cause crash */}
+				<Text className="font-bold text-sm">Welcome {auth.currentUser?.email}</Text>
 			</View>
+
+            <View>
+                <TouchableOpacity onPress={handleSignOut} className="mt-2 bg-red-600 rounded-lg">
+                    <Text className="text-white font-bold p-1.5">Sign Out</Text>
+                </TouchableOpacity>
+            </View>
 		</KeyboardAvoidingView>
 	);
 };
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,84 +1,116 @@
-import { useState } from 'react'
-import { Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useNavigation } from "@react-navigation/core";
+import {
+	Text,
+	View,
+	KeyboardAvoidingView,
+	TextInput,
+	TouchableOpacity,
+} from "react-native";
+import React from "react";
 
-import { auth } from '../../firebase'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-    const handleLogin = () => {
-        signInWithEmailAndPassword(auth, email, password)
-            .then(userCredentials => {
-                const user = userCredentials.user
-                console.log(user.email)
-            })
-            .catch(err => {
-                alert(`Email does not exist`)
-                console.log(err.message)
-            })
-    }
+	const navigation = useNavigation();
 
-    const handleRegistration = () => {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(userCredentials => {
-                const user = userCredentials.user
-                // console.log(user.email)
-            })
-            .catch(err => {
-                alert(`Email already in use`)
-                console.log(err.message)
-            })
-    }
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			if (user) {
+				// removes the log out button on the top left
+				// that appears if we use navigate
+				navigation.replace("Home");
+			}
+		});
+		// returning unsubscribe here stops the listener from being called
+		return unsubscribe;
+	}, []);
 
-    // console.log(password)
-    // console.log(email)
+	const handleLogin = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredentials) => {
+				const user = userCredentials.user;
+				if (user) {
+					navigation.navigate("Home");
+				}
+				// console.log(user.email)
+			})
+			.catch((err) => {
+				alert(`Incorrect email or password, try again`);
+				console.log(err.message);
+			});
+	};
 
-  return (
-    <KeyboardAvoidingView className="flex-1 justify-center items-center" behavior="padding">
-        {/* input container */}
-        <View className="w-[80%] bg-white rounded-lg mb-3">                        
-            <TextInput
-                className="p-2 placeholder:text-center"
-                placeholder='Email' 
-                value={email} 
-                onChangeText={text => setEmail(text)}
-            />
-        </View>
+	const handleRegistration = () => {
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredentials) => {
+				const user = userCredentials.user;
+			})
+			.catch((err) => {
+				alert(`Email already in use`);
+				console.log(err.message);
+			});
+	};
 
-        <View className="w-[80%] bg-white rounded-lg">
-        <TextInput       
-                className="p-2 placeholder:text-center"          
-                placeholder='Password'
-                secureTextEntry 
-                value={password} 
-                onChangeText={text => setPassword(text)}
-            />
-        </View>
+	return (
+		<KeyboardAvoidingView
+			className="flex-1 justify-center items-center"
+			behavior="padding"
+		>
+            <Text className="font-bold text-2xl mb-5">Login In</Text>
+			{/* input container */}
+			<View className="w-[80%] bg-white rounded-lg mb-3">
+				<TextInput
+					className="p-2 placeholder:text-center"
+					placeholder="Email"
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+				/>
+			</View>
 
-        {/* button container */}
-        <View className="w-[60%] justify-center items-center mt-10">
-            <TouchableOpacity
-                onPress={handleLogin}
-                className="bg-blue-600 p-2 mb-2 rounded-lg w-full"
-            >
-                <Text className="text-white text-center font-bold">
-                    Login
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity     
-                onPress={handleRegistration}           
-                className="bg-white p-2 rounded-lg w-full border-2 border-blue-600" 
-            >
-                <Text className="text-blue-600 text-center font-bold">
-                    Register
-                </Text>
-            </TouchableOpacity>
-        </View>
-    </KeyboardAvoidingView>
-  )
-}
+			<View className="w-[80%] bg-white rounded-lg">
+				<TextInput
+					className="p-2 placeholder:text-center"
+					placeholder="Password"
+					secureTextEntry
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+				/>
+			</View>
 
-export default LoginScreen
+			{/* button container */}
+			<View className="w-[60%] justify-center items-center mt-10">
+				<TouchableOpacity
+					onPress={handleLogin}
+					className="bg-blue-600 p-2 mb-2 rounded-lg w-full"
+				>
+					<Text className="text-white text-center font-bold">Login</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={handleRegistration}
+					className="bg-white p-2 rounded-lg w-full border-2 border-blue-600"
+				>
+					<Text className="text-blue-600 text-center font-bold">Register</Text>
+				</TouchableOpacity>
+
+				{/* TODO */}
+                <Text className="mt-2 font-bold">Or</Text>
+				<TouchableOpacity
+					// onPress={}
+					className="bg-white p-2 mb-2 rounded-lg w-full border-2 border-red-600 mt-2"
+				>
+					<Text className="text-red-600 text-center font-bold">Google</Text>
+				</TouchableOpacity>
+			</View>
+		</KeyboardAvoidingView>
+	);
+};
+
+export default LoginScreen;
