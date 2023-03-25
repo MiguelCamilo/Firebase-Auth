5fd1e6e5af7408057e33b03cd21ebf1aef38b3f4
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-25 13:01:03 -0400

    Starting google sign in with pop up

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -37,17 +37,18 @@ const LoginScreen = () => {
 	}, []);
 
 	const handleLogin = () => {
-        if (email.length || password.length <= 1) {
-            alert("Please enter a valid email and password!");
-        }
-
 		signInWithEmailAndPassword(auth, email, password)
 			.then((userCredentials) => {
 				const user = userCredentials.user;
 			})
 			.catch((err) => {
-				// alert(`Incorrect email or password, try again`);
-				console.log(err.message);
+                if (email.length || password.length <= 1) {
+                    alert("Please enter a valid email and password!");
+                } else {
+                    alert(`Incorrect email or password, try again`);
+                    console.log(err.message);
+                }        
+
 			});
 	};
 
